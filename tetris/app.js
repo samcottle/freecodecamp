5722009ae20349c289d9450c332e53ed805a5d8e
samcottle <30656767+samcottle@users.noreply.github.com>
2020-05-24 13:20:57 +0200

    Added styling and basic js

---
+++ tetris/app.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid')
+  let squares = Array.from(document.querySelectorAll('.grid div'))
+  const ScoreDisplay = document.querySelector('#score')
+  const StartBtn = document.querySelector('#start-button')
+  const Width = 10
+  // Up to 32:00 of https://youtu.be/rAUn1Lom6dw
+})
