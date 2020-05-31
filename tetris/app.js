9579b9f7d52e100be20848591509cacfe63998a7
samcottle <30656767+samcottle@users.noreply.github.com>
2020-05-31 20:57:45 +0200

    Defined TetrisShapes, and how to draw, in app.js

--- tetris/app.js
+++ tetris/app.js
@@ -4,5 +4,66 @@ document.addEventListener('DOMContentLoaded', () => {
   const ScoreDisplay = document.querySelector('#score')
   const StartBtn = document.querySelector('#start-button')
   const Width = 10
-  // Up to 32:00 of https://youtu.be/rAUn1Lom6dw
+
+  // Tetris shapes
+  const lShape = [
+    [1, Width + 1, Width * 2 + 1, 2],
+    [Width, Width + 1, Width + 2, Width * 2 + 2],
+    [1, Width + 1, Width * 2 + 1, Width * 2],
+    [Width, Width * 2, Width * 2 + 1, Width * 2 + 2]
+  ]
+
+  const zShape = [
+    [Width + 1, Width + 2, Width * 2, Width * 2 + 1],
+    [0, Width, Width + 1, Width * 2 + 1],
+    [Width + 1, Width + 2, Width * 2, Width * 2 + 1],
+    [0, Width, Width + 1, Width * 2 + 1]
+  ]
+
+  const tShape = [
+    [1, Width, Width + 1, Width + 2],
+    [1, Width + 1, Width + 2, Width * 2 + 1],
+    [Width, Width + 1, Width + 2, Width * 2 + 1],
+    [1, Width, Width + 1, Width * 2 + 1]
+  ]
+
+  const oShape = [
+    [0, 1, Width, Width + 1],
+    [0, 1, Width, Width + 1],
+    [0, 1, Width, Width + 1],
+    [0, 1, Width, Width + 1]
+  ]
+
+  const iShape = [
+    [1, Width + 1, Width * 2 + 1, Width * 3 + 1],
+    [Width, Width + 1, Width + 2, Width + 3],
+    [1, Width + 1, Width * 2 + 1, Width * 3 + 1],
+    [Width, Width + 1, Width + 2, Width + 3]
+  ]
+
+  const TetrisShapes = [lShape, zShape, tShape, oShape, iShape]
+
+  let currentPosition = 4
+  let currentRotation = 0
+
+  // Randomly select a shape and its first rotation
+  let random = Math.floor(Math.random() * TetrisShapes.length)
+  let current = TetrisShapes[random][currentRotation]
+
+  // Draw the shape
+  function draw() {
+    current.forEach(index => {
+      squares[currentPosition + index].classList.add('shape')
+    })
+  }
+
+  // Undraw the shape
+  function undraw() {
+    current.forEach(index => {
+      squares[currentPosition + index].classList.remove('shape')
+    })
+  }
+
+  // Up to https://www.youtube.com/watch?v=rAUn1Lom6dw&t=2732s
+
 })
